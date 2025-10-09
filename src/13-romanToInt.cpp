1b7e21694236189e364bad99d7b8368489a21a87
kwu130 <kwu130@gmail.com>
2025-10-09 23:48:17 +0800

    feat(src): add 13-romanToInt

---
+++ src/13-romanToInt.cpp
@@ -0,0 +1,47 @@
+//
+// Created by 1412 on 2025/10/9.
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> m(7);
+
+        m['I'] = 1;
+        m['V'] = 5;
+        m['X'] = 10;
+        m['L'] = 50;
+        m['C'] = 100;
+        m['D'] = 500;
+        m['M'] = 1000;
+
+        int res = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (i < s.size() - 1 && m[s[i]] < m[s[i + 1]]) {
+                res -= m[s[i]];
+            } else {
+                res += m[s[i]];
+            }
+        }
+
+        return res;
+    }
+};
+
+void test() {
+    Solution s;
+    assert(s.romanToInt("III") == 3);
+    assert(s.romanToInt("IV") == 4);
+    assert(s.romanToInt("IX") == 9);
+    assert(s.romanToInt("LVIII") == 58);
+    assert(s.romanToInt("MCMXCIV") == 1994);
+}
+
+int main() {
+    test();
+    return 0;
+}
